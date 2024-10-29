b94f4cb07f77daf823ff60efb2c6d11c8730e3a2
mo3es <baylomv@gmail.com>
2024-10-29 11:58:49 +0200

    done

--- address_book/address_book.py
+++ address_book/address_book.py
@@ -1,6 +1,14 @@
 from collections import UserDict
 
 
+"""
+    This programm provides managing of contacts.
+    Class address_book is manager - class, that provides creating, storing, deleting and finding contacts - by using functions with same names
+    Record class is an entity, that stores info about user`s name, phone numbers, etc. Its provides creation, phone number adding, deleting and changing, etc.
+
+"""
+
+
 class Field:
     def __init__(self, value):
         self.value = value
@@ -10,11 +18,11 @@ class Field:
 
 
 class Name(Field):
-    pass  
+    pass
 
 
 class Phone(Field):
-    pass  
+    pass
 
 
 class Record:
@@ -26,34 +34,39 @@ class Record:
         phone_instance = Phone(phone)
         if phone not in [p.value for p in self.phones]:
             self.phones.append(phone_instance)
-            print(f'Inputed phone number {phone} successfully added')
+            print(f"Inputed phone number {phone} successfully added")
         else:
-            print(f'Inputed phone number {phone} already exists')
+            print(f"Inputed phone number {phone} already exists")
 
     def del_phone(self, phone):
         for p in self.phones:
             if p.value == phone:
                 self.phones.remove(p)
-                print(f'Inputed phone number {phone} successfully deleted')
+                print(f"Inputed phone number {phone} successfully deleted")
                 return
-        print(f'Inputed phone number {phone} doesn`t exist')
+        print(f"Inputed phone number {phone} doesn`t exist")
 
     def edit_phone(self, phone, new_phone):
         for p in self.phones:
             if p.value == phone:
                 if new_phone not in [p.value for p in self.phones]:
                     p.value = new_phone
-                    print(f'Inputed phone number {phone} successfully edited to {new_phone}')
+                    print(
+                        f"Inputed phone number {phone} successfully edited to {new_phone}"
+                    )
                 else:
-                    print(f'Phone number {new_phone} already exists, phone number {phone} was not edited')
+                    print(
+                        f"Phone number {new_phone} already exists, phone number {phone} was not edited"
+                    )
                 return
-
 
     def find_phone(self, phone):
         if phone in [p.value for p in self.phones]:
-            print(f'In {self.name.value} contact info, phone number {phone} exists')
+            print(f"In {self.name.value} contact info, phone number {phone} exists")
         else:
-            print(f'In {self.name.value} contact info, phone number {phone} does not exist')
+            print(
+                f"In {self.name.value} contact info, phone number {phone} does not exist"
+            )
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
@@ -66,9 +79,9 @@ class AddressBook(UserDict):
     def add_record(self, record: Record):
         if record.name.value not in self.data:
             self.data[record.name.value] = record
-            print(f'Record {record.name.value} was successfully added')
+            print(f"Record {record.name.value} was successfully added")
         else:
-            print(f'Record {record.name.value} already exists')
+            print(f"Record {record.name.value} already exists")
 
     def find(self, name) -> Record:
         return self.data.get(name)
@@ -76,7 +89,6 @@ class AddressBook(UserDict):
     def delete(self, name):
         if name in self.data:
             del self.data[name]
-            print(f'Record of person {name} was successfully deleted')
+            print(f"Record of person {name} was successfully deleted")
         else:
-            print(f'Record for person {name} not found')
-
+            print(f"Record for person {name} not found")
--- address_book/test.py
+++ address_book/test.py
@@ -1,9 +1,12 @@
 import address_book
 
+"""
+    Test cases for address_book programm
+"""
+
 
 def main():
     book = address_book.AddressBook()
-
 
     john_record = address_book.Record("John")
     john_record.add_phone("1234567890")
@@ -21,18 +24,15 @@ def main():
     john = book.find("John")
     john.edit_phone("1234567890", "1112223333")
 
-
     print(john)
 
-
     john.find_phone("5555555555")
-    
-
 
     book.delete("Jane")
 
     for name, records in book.data.items():
         print(records)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
